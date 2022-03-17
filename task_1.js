332ad3458d2f3181b9ca16f1a9712a3f4001b86e
Sydney Abuya <69866777+SydneySmoothvile@users.noreply.github.com>
2022-03-17 08:52:34 +0300

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -1,11 +1,6 @@
 longestWord('Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers')
 function longestWord(text) {
-    var result = text.split(' ').reduce((maxLengthWord, currentWord) => {
-        if (currentWord.length > maxLengthWord.length) {
-            return currentWord
-        } else {
-            return maxLengthWord
-        }
-    }, "")
-    return result
+    var sortedArray = text.split(' ')
+                          .sort((wordA, wordB) => wordB.length - wordA.length)
+    return sortedArray[0]
 }
